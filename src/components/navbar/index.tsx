e0b424145a4e4df94efec869e2528a4728d48c39
fajar septiawan <fajarsep12dev@gmail.com>
2020-02-29 02:42:17 +0700

    feat: add layout

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+export default () => (
+  <Navbar bg="light" fixed="top">
+    <Navbar.Brand href="#home">Nano TIA</Navbar.Brand>
+  </Navbar>
+);
