bd39771461db81da003e112e82ae4b41a0d91ecc
fajar septiawan <fajarsep12dev@gmail.com>
2020-03-01 13:31:00 +0700

    fixing: remove image

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,15 +5,7 @@ import './style.css';
 export default () => (
   <Navbar fixed="top" className="light-nav">
     <Container>
-      <Navbar.Brand href="/">
-        <img
-          alt=""
-          src="/techinasia.png"
-          width="100"
-          height="30"
-          className="d-inline-block align-top"
-        />
-      </Navbar.Brand>
+      <Navbar.Brand href="/">NANOTIA</Navbar.Brand>
     </Container>
   </Navbar>
 );
